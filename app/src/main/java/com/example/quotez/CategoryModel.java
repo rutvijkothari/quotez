5cba8e5b579c5b442156b8362ecc9c6f9de7e00a
Rutvij Kothari <87492368+rutvijkothari@users.noreply.github.com>
2022-07-02 16:45:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quotez/CategoryModel.java
@@ -0,0 +1,29 @@
+package com.example.quotez;
+
+public class CategoryModel {
+    private String courseName;
+    private String courseDescription;
+
+    // creating constructor for our variables.
+    public CategoryModel(String courseName, String courseDescription) {
+        this.courseName = courseName;
+        this.courseDescription = courseDescription;
+    }
+
+    // creating getter and setter methods.
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
+}
